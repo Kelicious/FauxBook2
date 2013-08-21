46c5c82c868bd86887898e42c2781824700a6868
Ke Li <like@umich.edu>
2013-08-21 15:52:02 -0400

    Show message if album is empty

--- app/assets/javascripts/controllers/album_controller.js
+++ app/assets/javascripts/controllers/album_controller.js
@@ -3,12 +3,16 @@ App.AlbumController = Ember.ObjectController.extend({
 
   canEdit: function () {
     var currentUser = this.get('controllers.currentUser.model');
-    return currentUser.id == this.get('user.id');
-  },
+    return currentUser.get('id') == this.get('user.id');
+  }.property('user'),
 
   formAction: function () {
     return "/albums/" + this.get('id') + "/photos";
   }.property('model'),
+
+  hasNoPhotos: function () {
+    return this.get('photos.length') == 0;
+  }.property('photos'),
 
   uploadPhoto: function () {
     var self = this;
