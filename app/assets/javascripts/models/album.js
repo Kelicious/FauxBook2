7d724afdd5ac90ab19262f7a978a8b2cb1b55564
Ke Li <like@umich.edu>
2013-08-23 11:37:06 -0400

    Better style on albums page

--- app/assets/javascripts/models/album.js
+++ app/assets/javascripts/models/album.js
@@ -1,5 +1,18 @@
 App.Album = DS.Model.extend({
   title: DS.attr('string'),
   user: DS.belongsTo('App.User'),
-  photos: DS.hasMany('App.Photo')
+  photos: DS.hasMany('App.Photo'),
+
+  firstPhoto: function () {
+    return this.get('photos.firstObject');
+  }.property('photos', 'photos.firstObject'),
+
+  pictureUrl: function () {
+    var photo = this.get('firstPhoto');
+    if (photo && photo.get('pictureUrlSmall')) {
+      return photo.get('pictureUrlSmall');
+    } else {
+      return "/assets/missing.jpg";
+    }
+  }.property('firstPhoto.pictureUrlSmall')
 });
